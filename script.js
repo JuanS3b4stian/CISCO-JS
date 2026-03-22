fc7b139b3444e86f9b63fe9f4243fd95d806588c
Juan Sebastian <juan.mun3ra@gmail.com>
2026-03-22 00:14:50 -0500

    Module 6

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-console.log(miVariable); // undefined
-var miVariable = 5;
-
-miFuncion(); // "Hola"
-function miFuncion() {
-  console.log("Hola");
+function outer() {
+    let name = "outer";
+    let str = inner();
+    return str;
 }
+ 
+function inner() {
+    let name = "inner";
+    return "Hello !";
+}
+ 
+console.log("before outer() call");
+console.log(outer());
